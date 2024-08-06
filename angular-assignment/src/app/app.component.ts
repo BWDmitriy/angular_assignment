9ad2524500716945b4a2f7d857ad2c3c09abc7e5
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-08-06 18:28:35 +0300

    Initial files cleanup, +password-strength component
    Made a bit of cleanup of files which are included in base create app command. Also, created password-strength component (WiP)

--- angular-assignment/src/app/app.component.ts
+++ angular-assignment/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PasswordStrengthComponent } from './password-strength/password-strength.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PasswordStrengthComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-assignment';
